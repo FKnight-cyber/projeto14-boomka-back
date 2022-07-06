47c1852838b5e9005cc015687d61e0066fb7de04
emily <nakanodepaula@gmail.com>
2022-07-06 10:30:08 -0300

    western organization

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express, {json} from 'express';
 import router from './routes/index.js'
 import dotenv from 'dotenv';
+import cors from 'cors';
+
 dotenv.config()
 
 const app = express();
-app.use(json);
+app.use(json());
+app.use(cors())
 app.use(router)
 
-app.listen(5000);
+app.listen(process.env.PORT);
